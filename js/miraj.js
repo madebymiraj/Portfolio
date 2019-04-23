38126d8aeb8e421b83457c145b317efdaec9e758
Miraj Patel <miraj@madebymiraj.com>
2019-04-23 01:56:01 -0400

    Main case studies

---
+++ js/miraj.js
@@ -0,0 +1,15 @@
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+    document.getElementById("BackToTop").style.display = "block";
+  } else {
+    document.getElementById("BackToTop").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  $("html, body").animate({ scrollTop: 0 }, 600);
+}
